a39dc484c8b3ff39da5e7064c0d7926adc2725fc
zinx110 <tasdid110@gmail.com>
2023-10-30 01:21:56 +0600

    db names

---
+++ notes/page.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <?php include_once("components/header.php") ?>
+    <table border=0 width="100%">
+        <tr>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>A</td>
+        </tr>
+    </table>
+    <?php include_once("components/footer.php") ?>
+</body>
+
+</html>
