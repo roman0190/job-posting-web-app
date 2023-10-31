92530495b68b93ce667dd97ffd8c0df792ab3f80
WTMrittika <21-45108-2@student.aiub.edu>
2023-11-01 05:06:58 +0600

    rules and terms

---
+++ view/termsAll.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Terms Of Services</title>
+</head>
+<body>
+    <?php include_once("components/header.php") ?>
+
+    <h1>Terms Of Services</h1>
+    <p>The terms of services will be written here.</p>
+
+    <p><a href="landingPageAll.php">Back</a></p>
+
+    <?php include_once("components/footer.php") ?>
+</body>
+</html>
