771b5eee72f719ba63efc11483a1b389c3b5e24b
Roman Howladar <romanhowladar@Romans-MacBook-Pro.local>
2023-12-06 19:02:54 +0600

    roman

--- controller/Event/editCheck.php
+++ controller/Event/editCheck.php
@@ -1,4 +1,5 @@
 <?php
+
 include('../../model/eventModel.php');
 
 if (isset($_POST['submit'])) {
--- controller/dashboard/dashboardSession.php
+++ controller/dashboard/dashboardSession.php
@@ -1,30 +1,27 @@
 <?php
 
-session_start();
+    session_start();
 
-if (isset($_SESSION['userInfo'])) {
-    $userInfo = $_SESSION['userInfo'];
-
-    if (isset($userInfo['user_type'])) {
-        $role = $userInfo['user_type'];
-
-        if ($role === 'admin') {
-            header('location: ../../view/dashboard/dashboardAdmin.php');
-            
-        } else if ($role === 'applicant') {
-            header('location: ../../view/dashboard/dashboardApplicant.php');
-           
-        } else if ($role === 'recruiter') {
-            header('location: ../../view/dashboard/dashboardRecruiter.php');
-        
-        }
+    if (isset($_COOKIE['userInfo'])) {
+        $userInfo = (array) json_decode($_COOKIE['userInfo']);
     } else {
-        
-        echo "Error: 'user_type' not set in userInfo.";
+        session_start();
+        if (isset($_SESSION['userInfo'])) {
+            $userInfo = (array)  json_decode($_SESSION['userInfo']);
+        } 
     }
-} else {
-   
-    echo "Error: 'userInfo' not set in session.";
-}
 
+    $role = $userInfo['user_type'];
+
+    if ($role === 'admin') {
+        header('location: ../../view/dashboard/dashboardAdmin.php');
+        
+    } else if ($role === 'applicant') {
+        header('location: ../../view/dashboard/dashboardApplicant.php');
+        
+    } else if ($role === 'recruiter') {
+        header('location: ../../view/dashboard/dashboardRecruiter.php');
+    
+    }
 ?>
+
