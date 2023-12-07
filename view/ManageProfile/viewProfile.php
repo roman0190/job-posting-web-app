45af8f37203d0f8d69c314532a3cc75b3f56d97e
WTMrittika <21-45108-2@student.aiub.edu>
2023-12-07 18:50:42 +0600

    update

--- view/ManageProfile/viewProfile.php
+++ view/ManageProfile/viewProfile.php
@@ -46,7 +46,7 @@
                     &nbsp;&nbsp;&nbsp;&nbsp;MARZ JOB SITE
                 </h2>
                 <h5 align="right">
-                    <a href="../Auth/signIn.php">Sign out</a>
+                    <a href="../Auth/logout.php">Logout</a>
                 </h5>
             </th>
         </tr>
