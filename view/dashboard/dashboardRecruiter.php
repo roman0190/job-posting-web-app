807aa16d063881967ff16fefcd32a1fd3a150355
zinx110 <tasdid110@gmail.com>
2023-12-09 21:59:51 +0600

    up

--- view/dashboard/dashboardRecruiter.php
+++ view/dashboard/dashboardRecruiter.php
@@ -1,26 +1,29 @@
-<?php 
-    require_once('../../controller/Auth/session.php'); 
+<?php
+require_once('../../controller/Auth/session.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="../../assets/CSS/dashboard/dashboardRecruiter.css">
     <title>Job Posting Recruiter Dashboard</title>
 </head>
+
 <body>
-  <div class="container">
+    <div class="container">
         <div class="header">
             <div class='user-info'>
                 <img src="../../assets/images/profilePicture.jpg" alt="">
                 <span>Welcome,<a href="../ManageProfile/viewProfile.php"><b id='name'></b></a></span>
             </div>
-           <div class=head-content>
-                <h1>Dashboard</h1><h6 id='role'style="text-transform: uppercase;" ></h6>
-           </div>
-           <div class='logout'>
+            <div class=head-content>
+                <h1>Dashboard</h1>
+                <h6 id='role' style="text-transform: uppercase;"></h6>
+            </div>
+            <div class='logout'>
                 <a href="../Auth/logout.php">logout</a>
-           </div>
+            </div>
         </div>
 
         <div class="nav">
@@ -31,12 +34,12 @@
                 <li><a href="../Rules/rulesAll.php">Rules & Regulation</a></li>
                 <li><a href="#">FAQ</a></li>
                 <li><a href="../chat/chat.php">Chat</a></li>
-                <li><a href="#">Notifications</a></li>
+                <li><a href="../notification/notification.php">Notifications</a></li>
             </ul>
         </div>
 
         <div class="main">
-        <p> <a href="../ManageProfile/viewProfile.php">View Profile</a></p>
+            <p> <a href="../ManageProfile/viewProfile.php">View Profile</a></p>
             <p> <a href=".php">View All Applicants</a></p>
             <p> <a href=".php">View All Jobs</a></p>
             <p> <a href=".php">Create new Jobs</a></p>
@@ -45,9 +48,10 @@
 
         <div class="footer">
             @Copyright Job-posting-web-app
-        </div>  
-          
-</div>
+        </div>
+
+    </div>
 </body>
+
 </html>
 <script src="../../assets/JS/dashboard/dashboardRecruiter.js"></script>
